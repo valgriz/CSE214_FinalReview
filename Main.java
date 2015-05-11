953057d9733e7ba7fd34380b5e85f82772fd06e4
Steven Marturano <steven.marturano@gmail.com>
2015-05-11 00:27:51 -0400

    selection sort implemented

--- Main.java
+++ Main.java
@@ -31,7 +31,7 @@ public class Main {
 			printMenu();
 			break;
 		case "b":
-
+			selectionSort();
 			printMenu();
 			break;
 		case "q":
@@ -43,6 +43,35 @@ public class Main {
 			printMenu();
 			break;
 		}
+	}
+
+	public void selectionSort() {
+		int[] array = { 3, 10, 1, 2, 9, 8, 2, 5, 7, 6 };
+		System.out.println("Selection Sort:");
+		System.out.println(arrayToString(array));
+		int divider = 0;
+		int minNum = Integer.MAX_VALUE;
+		for (int i = 0; i < array.length; i++) {
+			minNum = Integer.MAX_VALUE;
+			divider = i;
+			int minIndex = divider;
+			for (int j = divider; j < array.length; j++) {
+				if (minNum > array[j]) {
+					minNum = array[j];
+					minIndex = j;
+				}
+				// Uncomment the following line out to view all n^2 outputs
+				// System.out.println(arrayToString(array));
+			}
+			int a = array[divider];
+			int b = array[minIndex];
+			array[minIndex] = a;
+			array[divider] = b;
+			System.out.println(arrayToString(array));
+		}
+		System.out
+				.println("Worst case time-complexity: O(n^2)\nTo see all n^2 outputs, uncomment out the System.out.print in bubbleSort()");
+		System.out.println();
 	}
 
 	public void bubbleSort() {
